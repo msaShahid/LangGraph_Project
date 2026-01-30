13b87a8c7eb6b7317baa0370d3a744f7b16f9395
Mohammad Shahid <msa.ansari28@gmail.com>
2026-01-30 22:00:33 +0530

    `refactor: improve agent setup clarity and maintainability`

--- agent/react_agent_basic.py
+++ agent/react_agent_basic.py
@@ -1,8 +1,9 @@
 from dotenv import load_dotenv
 
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.agents import initialize_agent, AgentType
+from langchain.agents import initialize_agent, AgentType, tool
 from langchain_community.tools import TavilySearchResults
+import datetime
 
 load_dotenv()
 
@@ -13,7 +14,14 @@ llm = ChatGoogleGenerativeAI(
 
 search_tool = TavilySearchResults(search_depth="basic")
 
-tools = [search_tool]
+@tool
+def get_system_time(format: str = "%Y-%m-%d %H:%M:%S"):
+  """Return the current date and time """
+  current_time = datetime.datetime.now()
+  formatted_time = current_time.strftime(format)
+  return formatted_time
+
+tools = [search_tool, get_system_time]
 
 agent = initialize_agent(
     tools=tools,
@@ -23,7 +31,11 @@ agent = initialize_agent(
 )
 
 response = agent.invoke(
-    "Give me some key features of Python programming language."
+    "List the major Python versions (e.g., Python 1.x, 2.x, 3.x milestones) with their official release dates. "
+    "Present the result in a clear table with columns: Version, Release Date, Notes. "
+    "Use the search tool to verify dates if needed. "
+    "Finally, append the current system date and time using the system time tool."
 )
+
 
 print(response)
